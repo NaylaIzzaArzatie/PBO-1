a0288c8e2e0c4c330a8565fdf638677cbfdcfdf9
NaylaIzzaArzatie <arzaizzati@gmail.com>
2025-09-01 23:52:58 +0700

    Add files via upload

---
+++ Tugas2PBO.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[]args) {
+        System.out.printin("Nama: Nayla");
+        System.out.printin("NPM: 2440506052");
+        System.out.printin("Alamat: Temanggung"):
+        System.out.printin("No_HP: 081234567890");
+    }
+}
